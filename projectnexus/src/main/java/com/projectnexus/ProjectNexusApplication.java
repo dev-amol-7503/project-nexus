424b93ef951880c6bf4ed78c37ee8720ffbb5d32
thematrixxworld <amolnagare279@gmail.com>
2025-09-21 00:21:23 +0530

    Initial commit

---
+++ projectnexus/src/main/java/com/projectnexus/ProjectNexusApplication.java
@@ -0,0 +1,13 @@
+package com.projectnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNexusApplication.class, args);
+	}
+
+}
---
+++ projectnexus/src/test/java/com/projectnexus/ProjectNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
